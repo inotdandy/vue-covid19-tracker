0d803f5cd94a5a987886887594bada47b22b7447
Dandy Inot <dandy.inot1990@gmail.com>
2021-09-10 17:34:27 +0800

    done covid19 tracker app

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,33 @@
+<template>
+  <select 
+    @change="onChange()"
+    class="mt-10 form-select block w-full border p-3 rounded" 
+    v-model="selected">
+      <option value="0">Select Country</option>
+      <option v-for="country in countries" :key="country.ID" :value="country.ID">{{ country.Country }}</option>
+  </select>
+</template>
+
+<script>
+export default {
+    name: 'CountrySelect',
+    data(){
+        return{
+            selected: 0
+        }
+    },
+    props: ['countries'],
+    methods: {
+        onChange(){
+            
+            const selectedCountry = this.countries.find(item => item.ID === this.selected)
+
+            this.$emit('get-country', selectedCountry)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/DataTitle.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="text-center">
+        <h2 class="text-3xl font-bold">{{ text }}</h2>
+        <div class="text-2xl mt-4 mb-10">{{ timestamps }}</div>
+    </div>
+</template>
+
+<script>
+import moment from 'moment'
+
+export default {
+    name:'DataTitle',
+    props: [
+        'text',
+        'dataDate'
+    ],
+    computed: {
+        timestamps: function() {
+            return moment(this.dataDate).format('MMMM Do YYYY, hh:mm:ss')
+        }
+    }
+}
+</script>
